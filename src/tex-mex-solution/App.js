b0820cb3827073d448129095add51fbb2267853d
fresh5447 <doug@kosmojo.com>
2017-11-08 14:21:23 -0700

    tex mex solution - where students left off in class yesterday

--- src/tex-mex-solution/App.js
+++ src/tex-mex-solution/App.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react'
+import data from './Data'
+import RestaurantInfo from './RestaurantInfo'
+import MenuContainer from './MenuContainer'
 
-class App extends Component {
+class TexMexApp extends Component {
   render () {
     return (
       <div>
-        <h3>Tex Mex App Rendering!</h3>
+        <RestaurantInfo
+          name={data.company[0].title}
+          address={data.address.street}
+          phone={data.phone}
+
+        />
+        <MenuContainer />
       </div>
     )
   }
 }
 
-export default App
+export default TexMexApp
