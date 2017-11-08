d40203a1aa547cc83dffd33e1242b92eb60d3022
fresh5447 <doug@kosmojo.com>
2017-11-08 13:34:53 -0700

    tex-mex-solution

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import App from './webApp/App'
-// import MonsterApp from './MonsterMash/App'
-// import SmartApp from './smartApp/App'
-// import MappingAndFiltering from './MappingAndFiltering/MappingAndFiltering'
-import ChatterApp from './ChatterApp/ChatterApp'
+import App from './tex-mex-solution/App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<ChatterApp />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
