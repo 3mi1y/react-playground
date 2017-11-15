9b76f657fefa7461ddb2706ea8ee65f5d0a5d0f4
Emily <emily@portal.org>
2017-11-15 15:15:02 -0700

    Fixed formatting on vanilla js warmup

--- src/filter-map-review/warmup.js
+++ src/filter-map-review/warmup.js
@@ -1,72 +1,77 @@
 // Vanilla js warmup/review for filtering and mapping
+// challenges created based off of an array of class objects
 
 const classes = [
   {
     name: 'Knitting for Beginners',
-    lengthInHours: 3,
+    length: 3,
     cost: 50,
     instructor: 'Suzy Knitter',
     location: 'Beads, Yarns and Threads'
   },
   {
     name: 'How to Tie a Tie',
-    lengthInHours: 1,
+    length: 1,
     cost: 100,
     instructor: 'Dr. Johnson',
     location: 'UC, 2nd floor, Room 101'
   },
   {
     name: 'Basket Weaving',
-    lengthInHours: 5,
+    length: 5,
     cost: 300,
     instructor: 'Phylis Taylor',
     location: 'Online'
   },
   {
     name: 'Intro to Guitar',
-    lengthInHours: 3,
+    length: 3,
     cost: 0,
     instructor: 'Professor Anderson',
     location: 'Music Building, Room 406'
   },
   {
     name: 'How to make Royal Icing for Sugar Cookies',
-    lengthInHours: 2,
+    length: 2,
     cost: 20,
     instructor: 'Ms. ButterBottom',
     location: 'The Good Food Store'
   },
   {
     name: 'FREE Resume Building Course',
-    lengthInHours: 2,
+    length: 2,
     cost: 0,
     instructor: 'TBD',
     location: 'UC, main floor'
   }
-];
+]
 
 // Challenge One - Use filter or map to return an array of class names
 const names = classes.map(x => {
   return x.name
 })
-console.log(names)
+// console.log(names)
+
 // Challenge Two - User filter or map to return an array of descriptions for each class
 const classInfo = classes.map(x => {
   return `Name: ${x.name}, Instructor: ${x.instructor}, Location: ${x.location}, Price: $${x.price}`
 })
-console.log(classInfo)
+// console.log(classInfo)
+
 // Challenge One - Use filter or map to return an array of the free classes
 const freeClasses = classes.filter(x => {
   return x.cost === 0
 })
-console.log(freeClasses)
+// console.log(freeClasses)
+
 // Challenge Two - User filter and/or map to return the name of the online classes
 const onlineClasses = classes.filter(x => {
   return x.location === 'Online'
 }).map(x => {
   return x.name
 })
-console.log(onlineClasses)
+// console.log(onlineClasses)
+
 // Challenge Three - Use filter and/or map to only return the names of classes that only take two hours or less and cost less than $50
 const shortCheapClasses = classes.filter(x => {
   return x.lengthInHours <= 2 && x.cost <= 50
